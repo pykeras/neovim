7b99e704986cb54e1e572100497a5fce8e841251
Mo Abbasi <aipythoner@gmail.com>
2025-01-02 01:08:23 +0330

    fix: remove watermark, disable lazy load

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,10 +1,11 @@
 return {
     "mistricky/codesnap.nvim",
     build = "make",
-    lazy = true,
+    lazy = false,
     opts = {
         save_path = "~/Pictures/",
         has_line_number = true,
         bg_color = "#535c68",
+        watermark = ""
     }
 }
