96b25f9d790681b808203f56656ad3b25b63534c
Mo Abbasi <aipythoner@gmail.com>
2025-06-13 16:11:52 +0330

    fix: linting, formatting issues on neovim v0.11.X

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,251 +1,247 @@
 return {
-    -- Mason: Installer for LSP servers and tools
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    -- Mason-LSPConfig: Auto-installer for LSP servers
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "ruff", "rust_analyzer", "jsonls", "yamlls", "dockerls" },
-            })
-        end
-    },
-    {
-        "b0o/schemastore.nvim",
-    },
-    -- NVIM-LSPConfig: LSP server configurations using blink-cmp capabilities
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { 'saghen/blink.cmp' },
-        config = function()
-            local capabilities = require('blink-cmp').get_lsp_capabilities()
-            local lspconfig = require('lspconfig')
-            vim.diagnostic.config({
-                signs = {
-                    text = {
-                        [vim.diagnostic.severity.ERROR] = " ",
-                        [vim.diagnostic.severity.WARN]  = " ",
-                        [vim.diagnostic.severity.HINT]  = " ",
-                        [vim.diagnostic.severity.INFO]  = " ",
-                    },
-                },
-                underline = true,
-                update_in_insert = true,
-                severity_sort = true,
-                virtual_text = false,
-                float = {
-                    border = "rounded",
-                    source = true,
-                    format = function(diagnostic)
-                        return diagnostic.message
-                    end,
-                    options = {
-                        wrap = true,
-                        linebreak = false,
-                    },
-                },
-            })
-            -- Lua
-            lspconfig.lua_ls.setup({
-                on_attach = function(_, bufnr)
-                    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-                end,
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                            path = vim.split(package.path, ';'),
-                        },
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false,
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
-            -- Pyright
-            lspconfig.pyright.setup({
-                on_attach = function(_, _)
-                    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-                        vim.lsp.diagnostic.on_publish_diagnostics, {
-                            virtual_text = true,
-                            signs = true,
-                            update_in_insert = true,
-                        }
-                    )
-                end,
-                on_init = function(client)
-                    local root_dir = client.config.root_dir
-                    local venv_path = root_dir .. "./venv"
-                    if vim.fn.isdirectory(venv_path) == 1 then
-                        client.config.settings.python.pythonPath = venv_path .. "/bin/python3"
-                    end
-                end,
-                filetype = { "python" },
-                capabilities = capabilities,
-                settings = {
-                    pyright = { disableOrganizeImports = true },
-                    python = {
-                        analysis = {
-                            typeCheckingMode = "off",
-                            diagnosticMode = "workspace",
-                            typeCheckingBehavior = "strict",
-                            reportMissingType = true,
-                            reportUnusedImport = false,
-                            reportUnusedVariable = false,
-                        },
-                    },
-                },
-            })
-            -- Rust
-            -- lspconfig.rust_analyzer.setup({
-            --     on_attach = function(_, bufnr)
-            --         vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-            --     end,
-            --     capabilities = capabilities,
-            --     settings = {
-            --         ["rust-analyzer"] = {
-            --             assist = { importGranularity = "module", importPrefix = "by_self" },
-            --             cargo = { allFeatures = true },
-            --             procMacro = { enable = true },
-            --         },
-            --     },
-            -- })
-            -- Ruff
-            lspconfig.ruff.setup({
-                on_attach = function(_, bufnr)
-                    vim.keymap.set("n", "<leader>rf", function()
-                        vim.lsp.buf.format({ async = false }) -- true: This causes a problem where the file must be saved multiple times.
-                    end, { buffer = bufnr, desc = "Format with Ruff" })
-                end,
-                capabilities = capabilities,
-                init_options = {
-                    settings = {
-                        showSyntaxErrors = true,
-                        lint = {
-                            extendSelect = { "I" }
-                        }
-                    }
-                }
-            })
-            -- JSON LSP configuration
-            lspconfig.jsonls.setup({
-                on_attach = function(_, bufnr)
-                    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-                end,
-                capabilities = capabilities,
-                settings = {
-                    json = {
-                        schemas = require('schemastore').json.schemas(),
-                        validate = { enable = true },
-                    },
-                },
-            })
-            -- Docker
-            lspconfig.dockerls.setup({
-                on_attach = function(_, bufnr)
-                    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-                end,
-                capabilities = capabilities,
-            })
+	-- Mason: Installer for LSP servers and tools
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-            --
-            -- Common autocommand: disable ruff hover
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
-                callback = function(args)
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if client and client.name == "ruff" then
-                        client.server_capabilities.hoverProvider = false
-                    end
-                end,
-                desc = "Disable hover for Ruff",
-            })
-            -- run Ruff autofix on save
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*.py",
-                callback = function()
-                    local clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() }) -- deprecated: but still do the job
-                    local ruff_client = nil
-                    for _, client in pairs(clients) do
-                        if client.name == "ruff" then
-                            ruff_client = client
-                            break
-                        end
-                    end
+	-- Mason-LSPConfig: Auto-installer for LSP servers
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"ruff",
+					"jsonls",
+					"yamlls",
+					"dockerls",
+				},
+			})
+		end,
+	},
 
-                    if ruff_client then
-                        vim.lsp.buf.code_action({
-                            context = { only = { "source.fixAll" } },
-                            apply = true,
-                            action_handler = function(action)
-                                if action.command then
-                                    action.command.arguments = { "--extend-select", "I" }
-                                end
-                                vim.lsp.buf.execute_command(action.command)
-                            end,
-                        })
-                    else
-                        vim.notify("Ruff LSP not available ...", vim.log.levels.WARN)
-                    end
-                end,
-                desc = "Run Ruff autofix and format on save for Python files",
-            })
-            -- Auto-format on saving any LSP client that supports formatting
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                callback = function()
-                    vim.lsp.buf.format({
-                        async = false, -- true: This causes a problem where the file must be saved multiple times.
-                        filter = function(client)
-                            return client.supports_method("textDocument/formatting")
-                        end,
-                    })
-                end,
-                desc = "Auto-format on save with specific LSP clients",
-            })
-        end
-    },
-    {
-        "nvimtools/none-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.diagnostics.mypy.with({ extra_args = { "--strict", "--show-error-codes", "--exclude", ".venv" } }),
-                    null_ls.builtins.formatting.prettier.with({
-                        filetypes = {
-                            "jsonl", "yaml", "yml",
-                            "html", "css", "scss",
-                            "javascript", "javascriptreact",
-                            "typescript", "typescriptreact",
-                        },
-                    }),
-                },
-                on_attach = function(client, bufnr)
-                    if client.supports_method("textDocument/formatting") then
-                        local aug = vim.api.nvim_create_augroup("none_ls_format", { clear = true })
-                        vim.api.nvim_clear_autocmds({ group = aug, buffer = bufnr })
-                        vim.api.nvim_create_autocmd("BufWritePre", {
-                            group = aug,
-                            buffer = bufnr,
-                            callback = function() vim.lsp.buf.format({ bufnr = bufnr }) end,
-                        })
-                    end
-                end,
-            })
-        end,
-    }
+	-- mason-tool-installer for non-LSP tools ⚙️
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"mypy",
+					"prettierd",
+					"stylua",
+				},
+				auto_update = false,
+				run_on_start = true,
+				start_delay = 3000,
+			})
+		end,
+	},
+
+	{ "b0o/schemastore.nvim" },
+
+	-- NVIM-LSPConfig: LSP server configurations
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
+		config = function()
+			local capabilities = require("blink-cmp").get_lsp_capabilities()
+			local lspconfig = require("lspconfig")
+
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = " ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+				},
+				underline = true,
+				update_in_insert = true,
+				severity_sort = true,
+				-- virtual_text = false,
+				virtual_text = true,
+				float = {
+					border = "rounded",
+					source = true,
+					format = function(diagnostic)
+						return diagnostic.message
+					end,
+					options = {
+						wrap = true,
+						linebreak = false,
+					},
+				},
+			})
+
+			-- Lua
+			lspconfig.lua_ls.setup({
+				on_attach = function(_, bufnr)
+					vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+				end,
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
+						telemetry = { enable = false },
+					},
+				},
+			})
+
+			-- Pyright
+			lspconfig.pyright.setup({
+				on_init = function(client)
+					local root_dir = client.config.root_dir
+					local venv_path = root_dir and (root_dir .. "/.venv") or ""
+					if vim.fn.isdirectory(venv_path) == 1 then
+						client.config.settings.python.pythonPath = venv_path .. "/bin/python3"
+					end
+				end,
+				filetypes = { "python" },
+				capabilities = capabilities,
+				settings = {
+					python = {
+						analysis = {
+							typeCheckingMode = "on",
+							autoSearchPaths = true,
+							useLibraryCodeForTypes = true,
+							diagnosticMode = "workspace",
+						},
+					},
+				},
+			})
+
+			-- Ruff
+			lspconfig.ruff.setup({
+				on_attach = function(client, bufnr)
+					client.server_capabilities.hoverProvider = false
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+
+					vim.keymap.set("n", "<leader>rf", function()
+						require("conform").format({ bufnr = bufnr })
+					end, { buffer = bufnr, desc = "Format with Conform" })
+				end,
+				settings = {
+					lint = {
+						extendSelect = { "I" },
+					},
+				},
+				capabilities = capabilities,
+			})
+
+			-- JSON LSP configuration
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			})
+
+			-- Docker
+			lspconfig.dockerls.setup({ capabilities = capabilities })
+		end,
+	},
+
+	-- Linting with nvim-lint
+	-- {
+	-- 	"mfussenegger/nvim-lint",
+	-- 	event = { "BufReadPre", "BufNewFile" },
+	-- 	config = function()
+	-- 		local lint = require("lint")
+	--
+	-- 		lint.linters_by_ft = {
+	-- 			python = { "mypy" },
+	-- 		}
+	--
+	-- 		-- Dynamically configure mypy to use the project's virtual environment
+	-- 		lint.linters.mypy = {
+	-- 			-- Use a function to build the arguments dynamically
+	-- 			fn_args = function(opts)
+	-- 				-- Find the virtual environment directory
+	-- 				local venv_path = vim.fn.findup(".venv", opts.fname .. ";")
+	--
+	-- 				local args = {
+	-- 					"--strict",
+	-- 					"--show-error-codes",
+	-- 					"--exclude",
+	-- 					".venv",
+	-- 				}
+	--
+	-- 				-- If a .venv is found, tell mypy to use its python executable
+	-- 				if venv_path and venv_path ~= "" then
+	-- 					table.insert(args, "--python-executable")
+	-- 					table.insert(args, venv_path .. "/bin/python3")
+	-- 				end
+	--
+	-- 				-- Add the filename to be linted at the end
+	-- 				table.insert(args, opts.fname)
+	-- 				return args
+	-- 			end,
+	-- 		}
+	--
+	-- 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	-- 			group = vim.api.nvim_create_augroup("linting_au", { clear = true }),
+	-- 			callback = function(args)
+	-- 				lint.try_lint(nil, { bufnr = args.buf })
+	-- 			end,
+	-- 		})
+	-- 	end,
+	-- },
+
+	-- Formatting with conform.nvim
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			notify_on_error = false,
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- This single formatter will fix lint errors (including isort) and format the code.
+				python = { "ruff_format", "ruff_fix" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				css = { "prettierd" },
+				scss = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				jsonl = { "prettierd" },
+				yaml = { "prettierd" },
+				rust = { "rustfmt" },
+			},
+			-- Define the custom ruff_fix formatter
+			formatters = {
+				-- 1. A formatter that ONLY runs 'ruff format'
+				ruff_format = {
+					command = "ruff",
+					args = { "format", "--", "$FILENAME" },
+					stdin = false,
+				},
+				-- 2. A formatter that runs 'ruff check --fix'
+				ruff_check_fix = {
+					command = "ruff",
+					-- The --extend-select I argument ensures import sorting is always applied.
+					-- 'check --fix' applies all fixable lint rules.
+					args = { "check", "--fix", "--extend-select", "I", "--", "$FILENAME" },
+					stdin = false,
+				},
+			},
+		},
+	},
 }
--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,29 +1,34 @@
 return {
-    'mrcjkb/rustaceanvim',
-    version = '^6', -- Recommended
-    lazy = false,   -- This plugin is already lazy
-    config = function()
-        local data = vim.fn.stdpath("data")
-        local pkg_base = data .. "/mason/packages/codelldb/extension"
-        local codelldb_path = pkg_base .. "/adapter/codelldb"
-        local liblldb_path = pkg_base .. "/lldb/lib/liblldb.so"
+	"mrcjkb/rustaceanvim",
+	version = "^6", -- Recommended
+	lazy = false, -- This plugin is already lazy
+	config = function()
+		local data = vim.fn.stdpath("data")
+		local pkg_base = data .. "/mason/packages/codelldb/extension"
+		local codelldb_path = pkg_base .. "/adapter/codelldb"
+		local liblldb_path = pkg_base .. "/lldb/lib/liblldb.so"
 
-        local cfg = require('rustaceanvim.config')
-        vim.g.rustaceanvim = {
-            dap = {
-                adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-            },
-            server = {
-                auto_attach = function(_, bufnr)
-                    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-                end
-            },
-            tools = {
-                use_telescope = true,
-                inlay_hints = { enabled = true },
-            },
-        }
+		local cfg = require("rustaceanvim.config")
+		vim.g.rustaceanvim = {
+			server = {
+				capabilities = require("blink-cmp").get_lsp_capabilities(),
+				default_settings = {
+					["rust-analyzer"] = {
+						cargo = { buildScripts = { enable = true } },
+						procMacro = { enable = true },
+						check = { command = "clippy" },
+					},
+				},
+			},
+			dap = {
+				adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+			},
+			tools = {
+				use_telescope = true,
+				inlay_hints = { enabled = true },
+			},
+		}
 
-        vim.lsp.inlay_hint.enable(true)
-    end,
+		vim.lsp.inlay_hint.enable(true)
+	end,
 }
