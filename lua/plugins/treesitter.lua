8b927424d17d5b3a45112971a7dd999a99725656
Mo Abbasi <aipythoner@gmail.com>
2026-02-10 12:02:58 +0330

    update: support latest re-write 'main' of treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,46 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
+		local ok, ts = pcall(require, "nvim-treesitter")
+		if not ok then
+			vim.notify("nvim-treesitter not available", vim.log.levels.WARN)
+			return
+		end
+
+		ts.setup({})
+
+		local parsers = {
+			"lua",
+			"python",
+			"json",
+			"yaml",
+			"dockerfile",
+			"typescript",
+			"javascript",
+			"tsx",
+			"html",
+			"css",
+			"sql",
+			"bash",
+		}
+
+		pcall(ts.install, parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = parsers,
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+
+		local indent_ft = { "lua", "python", "javascript", "typescript", "tsx", "bash" }
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = indent_ft,
+			callback = function()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
-	dependencies = {
-		{
-			"OXY2DEV/markview.nvim",
-			lazy = false,
-		},
-	},
 }
