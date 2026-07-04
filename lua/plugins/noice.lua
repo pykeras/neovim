091888b227f95070f18368833307258ede80305a
Mo Abbasi <aipythoner@gmail.com>
2026-07-04 13:19:51 +0330

    fix: vim.notify overwritten issue on latest noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,28 +1,31 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
-        -- "nvim-treesiter/nvim-treesiter",
-        "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-        require("noice").setup({
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                },
-            },
-            presets = {
-                bottom_search = false,
-                command_palette = false,
-                long_message_to_split = true,
-                inc_rename = false,
-                lsp_doc_border = false,
-            },
-        })
-    end
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+		-- "nvim-treesiter/nvim-treesiter",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("noice").setup({
+			notify = {
+				enabled = false,
+			},
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					-- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			presets = {
+				bottom_search = false,
+				command_palette = false,
+				long_message_to_split = true,
+				inc_rename = false,
+				lsp_doc_border = false,
+			},
+		})
+	end,
 }
